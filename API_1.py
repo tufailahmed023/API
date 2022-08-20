be5acbf1709dfcb08a1782307cc1a1af25295a9e
tufailahmed023 <tufailahmed023@gmail.com>
2022-08-20 16:31:14 +0530

    API 1

--- API_1.py
+++ API_1.py
@@ -8,6 +8,48 @@ def test1():
         b = request.json['num2']
         result = a+b
         return  jsonify(str(result))
+@app.route('/abc1',methods=['GET','POST'])
+def test2():
+    if (request.method =='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a*b
+        return  jsonify(str(result))
+
+@app.route('/abc2',methods=['GET','POST'])
+def test6():
+    if (request.method =='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a-b
+        return  jsonify(str(result))
+
+
+@app.route('/abc3',methods=['GET','POST'])
+def test3():
+    if (request.method =='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a/b
+        return  jsonify(str(result))
+
+
+@app.route('/abc4',methods=['GET','POST'])
+def test4():
+    if (request.method =='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a**b
+        return  jsonify(str(result))
+
+
+@app.route('/abc5',methods=['GET','POST'])
+def test5():
+    if (request.method =='POST'):
+        a = request.json['num1']
+        b = request.json['num2']
+        result = a//b
+        return  jsonify(str(result))
 
 if __name__=='__main__':
     app.run()
